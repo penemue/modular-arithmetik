7c1e8dff3d84de55a0ad1015a810a714a9621d03
Vyacheslav Lukianov <lvo@intellij.net>
2017-01-06 23:53:19 +0300

    license (apache 2) header added to all the sources, dependencies updated

--- src/main/kotlin/com/github/penemue/modular$arithmetik/barrett/BarrettReduction.kt
+++ src/main/kotlin/com/github/penemue/modular$arithmetik/barrett/BarrettReduction.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 - 2017 Vyacheslav Lukianov (https://github.com/penemue)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.penemue.`modular$arithmetik`.barrett
 
 import com.github.penemue.`modular$arithmetik`.shl
@@ -10,7 +25,7 @@ object BarrettReduction {
     private val inverses = ConcurrentHashMap<BigInteger, BigInteger>()
 
     /**
-     * @return `n` % `m` using Barrett reduction algorithm (https://en.wikipedia.org/wiki/Barrett_reduction)
+     * @return `n` % `m` using Barrett reduction (https://en.wikipedia.org/wiki/Barrett_reduction)
      */
     fun barrettRemainder(n: BigInteger, m: BigInteger): BigInteger {
         val dividendLength = n.bitLength()
@@ -30,6 +45,5 @@ object BarrettReduction {
         var result = ((n * inverse) shr (modLength * 2 + 1)) * m
         result = n - result
         return if (result >= m) result - m else result
-
     }
 }
--- src/test/kotlin/com/github/penemue/modular$arithmetik/RhoTest.kt
+++ src/test/kotlin/com/github/penemue/modular$arithmetik/RhoTest.kt
@@ -1,3 +1,18 @@
+/**
+ * Copyright 2016 - 2017 Vyacheslav Lukianov (https://github.com/penemue)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.penemue.`modular$arithmetik`
 
 import org.junit.Test
@@ -9,11 +24,11 @@ class RhoTest {
      * Pollard's Monte Carlo *rho*-method for integer factorization applied to F8, the 8th Fermat number.
      * In 1980, a faster version of the algorithm was used to factor F8, and the factorization took 2 hours
      * on Univac 1100/42 ([http://maths-people.anu.edu.au/~brent/pd/rpb061.pdf]).
-     * Nowadays it takes ~40 seconds on a commodity computer.
+     * Nowadays it takes less than half a minute on a commodity computer.
      */
     @Test
     fun factorF8() {
-        val F8 = BigInteger.ONE.shiftLeft(256) + 1
+        val F8 = (BigInteger.ONE shl 256) + 1
         val one = BigInteger.ONE
         var x1 = one
         var x2 = one
